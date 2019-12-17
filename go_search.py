705b3a8e71ec1e9620b3f88b659c6e34c1524387
romanmendelproject <romanmendel777@gmail.com>
2019-12-17 20:33:24 +0300

    3

--- go_search.py
+++ go_search.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 import sys
 from search.google_search import pars_google
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Ошибка. Слишком мало параметров!")
-        sys.exit (1)
+        sys.exit(1)
     elif len(sys.argv) == 2:
-        pars_google(sys.argv[1],0)
+        pars_google(sys.argv[1], 0)
     elif len(sys.argv) == 3:
         try:
-            pars_google(sys.argv[1],int(sys.argv[2]))
+            pars_google(sys.argv[1], int(sys.argv[2]))
         except ValueError:
             print("Ошибка. Укажите целое число!")
             sys.exit(1)
--- search/google_search.py
+++ search/google_search.py
@@ -1,48 +1,53 @@
-import requests,sys
+import requests
 from bs4 import BeautifulSoup
+from constants import User_Agent
+
 
 def request(url):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {'User-Agent': User_Agent}
     response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content, "lxml" )
-    return soup
+    return BeautifulSoup(response.content, "lxml")
 
-def recursion(link,req_num):
-    soup_req=request(link)
-    links_req=list()
-    link_number=0
+
+def recursion(link, req_num):
+    soup_req = request(link)
+    links_req = list()
+    link_number = 0
     for link_req in soup_req.find_all('a'):
         if req_num > link_number:
             link_req_item = link_req.get('href')
-            if len(str(link_req_item))>4 and link_req_item[:4] == 'http':
+            if len(str(link_req_item)) > 4 and link_req_item[:4] == 'http':
                 links_req.append(link_req_item)
-                link_number+=1
+                link_number += 1
             else:
                 continue
         else:
             break
     return links_req
 
-def pars_google(search_text,number_reursion):
+
+def pars_google(search_text, number_reursion):
     with open('result.txt', 'w', encoding='utf-8') as output_file:
-        url='https://www.google.com/search?q={}'.format(search_text)
-        soup=request(url)
+        url = 'https://www.google.com/search?q={}'.format(search_text)
+        soup = request(url)
         if number_reursion:
-            for i,tag in enumerate(soup.find_all("div", {"class": "g"})):
-                if i<7:
+            for i, tag in enumerate(soup.find_all("div", {"class": "g"})):
+                if i < 7:
                     link = tag.find('a').get('href')
-                    result_main = "ОПИСАНИЕ:\n{}\nОСНОВНАЯ ССЫЛКА:\n{}\n".format(tag.text,link)
+                    result_main = f"ОПИСАНИЕ:\n{tag.text}\n\
+                    ОСНОВНАЯ ССЫЛКА:\n{link}\n"
                     output_file.write(result_main)
-                    rec = recursion(link,number_reursion)
+                    rec = recursion(link, number_reursion)
                     output_file.write("РЕКУРСИВНЫЕ ССЫЛКИ:\n")
                     for rec_item in rec:
-                        output_file.write("{}\n".format(rec_item))
+                        output_file.write(f"{rec_item}\n")
                     output_file.write("\n\n")
                 else:
                     break
         else:
-            for i,tag in enumerate(soup.find_all("div", {"class": "g"})):
-                if i<7:
+            for i, tag in enumerate(soup.find_all("div", {"class": "g"})):
+                if i < 7:
                     link = tag.find('a').get('href')
-                    result_main = "ОПИСАНИЕ:\n{}\nОСНОВНАЯ ССЫЛКА:\n{}\n\n".format(tag.text,link)
+                    result_main = f"ОПИСАНИЕ:\n{tag.text}\n\
+                    ОСНОВНАЯ ССЫЛКА:\n{link}\n\n"
                     output_file.write(result_main)
